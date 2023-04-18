0c91dc393de9ffee74de02e518fb1a934d009603
urlunaticguy <85105608+urlunaticguy@users.noreply.github.com>
2023-04-18 18:52:22 +0530

    fixing error

--- pages/frontend/components/Dashboard/JobsTable.js
+++ pages/frontend/components/Dashboard/JobsTable.js
@@ -25,13 +25,13 @@ export default function JobsTable(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {props.rows.length == 0 && (
+          {/* {props.rows.length == 0 && (
             <div style={{ padding: "1rem" }}>
               <p style={{ color: "black", fontFamily: "fantasy" }}>
                 You have not posted any jobs.
               </p>
             </div>
-          )}
+          )} */}
           {props.rows.map((row, index) => (
             <TableRow
               className={styles.row}
