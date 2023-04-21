eb64c066766610795c6077fba49aa419a82bae91
Bharat155 <52926295+Bharat155@users.noreply.github.com>
2023-04-21 10:43:56 +0530

    added name parameter that will come in login as a response

--- pages/api/v1/user/login.js
+++ pages/api/v1/user/login.js
@@ -6,42 +6,40 @@ import jwt from "jsonwebtoken";
 import User from "../../../../models/user";
 import connectDB from "../../db";
 
-
 export default async function login(req, res) {
-    await connectDB();
-  
-    try {
-      const { email, password } = req.body;
-  
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(400).json({
-          status: 400,
-          message: "User with this email does not exist",
-        });
-      }
-  
-      if (!bcryptjs.compareSync(password, user.password)) {
-        return res.status(400).json({
-          status: 400,
-          message: "Incorrect password",
-        });
-      }
-  
-      const token = jwt.sign({ id: user._id }, "passwordKey");
-  
-      res.json({
-        token,
-        status: 200,
-        message: "success",
-        data: { id: user._id },
-      });
-    } catch (e) {
-      res.status(500).json({
-        status: 500,
-        message: e.message,
+  await connectDB();
+
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        status: 400,
+        message: "User with this email does not exist",
       });
     }
+
+    if (!bcryptjs.compareSync(password, user.password)) {
+      return res.status(400).json({
+        status: 400,
+        message: "Incorrect password",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+
+    res.json({
+      token,
+      status: 200,
+      message: "success",
+      data: { id: user._id, name: user.name },
+    });
+  } catch (e) {
+    res.status(500).json({
+      status: 500,
+      message: e.message,
+    });
   }
-  
+}
