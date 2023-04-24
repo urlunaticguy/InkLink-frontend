a8d12b2ef1a824edf296371b451efe06d2ae6dd5
Bharat155 <52926295+Bharat155@users.noreply.github.com>
2023-04-25 00:53:15 +0530

    password removal

--- pages/api/v1/user.js
+++ pages/api/v1/user.js
@@ -1,4 +1,4 @@
-//get all the users with a query params limit and skip 
+//get all the users with a query params limit and skip
 //with an api endpoint /api/v1/user?limit=2&skip=0
 
 import connectDb from "../db";
@@ -16,10 +16,11 @@ export default async function getUsers(req, res) {
     });
   }
 
-  const { skip,limit } = query;
+  const { skip, limit } = query;
 
   try {
     const users = await User.find()
+      .select("-password -bookmarks") // exclude the password field from the response
       .skip(parseInt(skip))
       .limit(parseInt(limit))
       .lean(); // add .lean() to improve performance by returning plain JavaScript objects instead of Mongoose documents
